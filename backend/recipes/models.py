77c81c9dc711fbac3aa3b201cc2fccc622a6ea50
Андрей Цай <skaeris@yandex.ru>
2023-11-02 19:06:30 +0300

    Update user, recioe models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -13,6 +13,7 @@ class Tag(models.Model):
     class Meta:
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
+
     def __str__(self) -> str:
         return self.name
 
@@ -20,11 +21,12 @@ class Tag(models.Model):
 class Ingredient(models.Model):
     name = models.CharField('Название', max_length=256)
     unit_of_mesurement = models.CharField('Единицика измерения', max_length=256)
-    
+
     class Meta:
         verbose_name = 'Ингридиент'
         verbouse_name_plural = 'Ингридиенты'
         ordering = ['name']
+
     def __str__(self) -> str:
         return f'{self.name}, {self.unit_of_mesurement}'
 
@@ -36,9 +38,23 @@ class Recipe(models.Model):
     name = models.CharField('Название',max_length=256)
     author = models.ForeignKey(User, related_name='recipes',
                                verbose_name='Автор', on_delete=models.CASCADE)
-    ingridients = 
+    ingridients = models.ForeignKey(Ingredient)
     text = models.CharField('Описание рецепта', max_length=256)
     image = models.ImageField('Изображение',
-                             blank=True, null=True, upload_to='static/recipe/')
-    cooking_time = models.IntegerField > 1
+                              blank=True, null=True, upload_to='static/recipe/')
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления')
+    pub_date = models.DateField('Дата публикации', auto_now=True)
 
+
+class IngredientToRecipe():
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='recipe')
+    ingridient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
+                                   related_name='ingredient')
+    amount = models.IntegerField(default=1, verbose_name='Количество')
+
+    class Meta:
+        verbose_name = 'Количесво ингридиента'
+        verbose_name_plural = 'Количесво игриентов'
+        ordering = ['id']
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField('Email',
+                              max_length=256, unique=True)
+    first_name = models.CharField('Имя', max_length=256)
+    last_name = models.CharField('Фамилия', max_length=256)
+
+    class Meta():
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        oredring = ('id')
+
+    def __str__(self) -> str:
+        return self.email
