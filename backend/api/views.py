7d8118cff57f78c9634c3daac0598ae7dc041c94
Андрей Цай <skaeris@yandex.ru>
2023-11-10 16:50:35 +0300

    One more

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from recipes.models import (Tag, Ingredient, Recipe, IngredientToRecipe)
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewset
 
-# Create your views here.
+from .permissions import IsAdminorReadOnly, IsAuthorOrReadOnly
+
+
+class TagViewSet(ReadOnlyModelViewset):
+    queryset = Tag.objects.all()
+    serializer_class =
+    permission_classes =  (IsAdminorReadOnly, )
+
+class IngredientViewSet(ReadOnlyModelViewset):
+    queryset = Ingredient.objects.all()
+    serializers_class = 
+    permission_classes = (IsAdminorReadOnly,)
+    #fileter#
+
+class RecipeViewSet(ModelViewSet):
+    querysset = Recipe.objects.all()
+    permission_classes = (IsAuthorOrReadOnly | IsAdminorReadOnly, )
+    #pagination
+    #fileter#
