d4428033580675bc0b8f221ef3af0407e452e7ff
Андрей Цай <skaeris@yandex.ru>
2023-12-04 01:26:44 +0300

    Change view favorite again_4

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -84,8 +84,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def del_favorite(self, request, **kwargs):
         recipe = Recipe.objects.get(id=kwargs['pk'])
         favorite = Favorites.objects.get(
-            user=request.user, recipe=recipe).exists()
-        if not favorite:
+            user=request.user, recipe=recipe)
+        if not favorite.exists():
             return Response('Этот рецепт еще не в списке избранного.',
                             status=status.HTTP_400_BAD_REQUEST)
         favorite.delete()
