2ed28c8c4d4d0204d01981b1796bd3026d3ddb9f
Андрей Цай <skaeris@yandex.ru>
2023-12-05 03:28:51 +0300

    Change view serializer first try_15

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -11,7 +11,6 @@ router_users_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router_users_v1.urls)),
-    path('auth/token/login/', CustomAuthToken.as_view()),
     path('auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
