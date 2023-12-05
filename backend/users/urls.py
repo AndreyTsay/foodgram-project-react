5b446fa622f2195c09d1241604069726911eb10d
Андрей Цай <skaeris@yandex.ru>
2023-12-05 03:32:29 +0300

    Change view serializer first try_16

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
-from .views import CustomAuthToken, UserViewSet
+from .views import UserViewSet
 
 app_name = 'users'
 
