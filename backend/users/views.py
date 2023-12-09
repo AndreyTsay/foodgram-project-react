c39dbf86e6f340004dc162e18ebf44f6c982d646
Андрей Цай <skaeris@yandex.ru>
2023-12-09 09:10:48 +0300

    fith rewiew_1.32

--- backend/users/views.py
+++ backend/users/views.py
@@ -68,7 +68,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def del_subscribe(self, request, **kwargs):
         author = get_object_or_404(User, id=kwargs['id'])
         subscription = Subscription.objects.filter(
-            user=request.user, author=author)
+            user=request.user, author=author).first()
         if subscription.exists():
             subscription.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
