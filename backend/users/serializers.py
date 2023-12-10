b41ea468b0550cb71de3cf7c546f0fba0d069f47
Андрей Цай <skaeris@yandex.ru>
2023-12-10 21:06:12 +0300

    Another try to deploy_17/26

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,7 +2,7 @@ import re
 
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
-from django.shortcuts import get_object_or_404
+
 from djoser.conf import settings
 from djoser.serializers import TokenCreateSerializer, UserSerializer
 from rest_framework import fields, serializers, status
