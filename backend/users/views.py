daf551aa8bbea6b8c99a3a90c6d54c1ab0f3e483
Андрей Цай <skaeris@yandex.ru>
2023-12-12 17:48:51 +0300

    Another user view change_14

--- backend/users/views.py
+++ backend/users/views.py
@@ -83,6 +83,7 @@ class UserViewSet(viewsets.ModelViewSet):
             Subscription.objects.create(user=request.user, author=author)
             return Response(serializer.data,
                             status=status.HTTP_201_CREATED)
+
     @subscribe.mapping.delete
     def del_subscribe(self, request, **kwargs):
         author = get_object_or_404(User, id=kwargs['pk'])
