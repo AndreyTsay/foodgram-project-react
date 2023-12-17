7e44599b8e14234622ac67bbdfb44216f82ee301
Андрей Цай <skaeris@yandex.ru>
2023-12-17 19:17:03 +0300

    ABCDEFGHJILNMQйqqqqqqqqqqqqqqqqqqqwwerqe

--- backend/recipes/management/commands/dataloader.py
+++ backend/recipes/management/commands/dataloader.py
@@ -1,34 +1,21 @@
 import csv
+import os
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
-
-DATA = {Ingredient: 'ingredients.csv'}
+from recipes.models import Ingredients
 
 
 class Command(BaseCommand):
-    help = 'Import data from file to DB'
-
-    def import_data_from_csv_file(self):
-        for model, csv_file in DATA.items():
-            with open(
-                    f'{settings.BASE_DIR}/data/{csv_file}',
-                    'r',
-                    encoding='utf-8',
-            ) as file:
-                reader = csv.DictReader(file)
-                objects_to_create = []
-                for data in reader:
-                    objects_to_create.append(
-                        model(
-                            name=data['name'],
-                            measurement_unit=data['measurement_unit']
-                        )
-                    )
-                model.objects.bulk_create(objects_to_create)
-        self.stdout.write(self.style.SUCCESS('Successfully loaded data'))
+    help = 'Импорт данных из csv для модели Ingredients.'
 
     def handle(self, *args, **options):
-        self.import_data_from_csv_file()
+        file_path = os.fspath('data/ingredients.csv')
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            try:
+                Ingredients.objects.bulk_create([Ingredients(
+                    name=name, measurement_unit=measurement_unit)
+                    for name, measurement_unit in reader])
+            except Exception as error:
+                print(f'Ошибка:{error}')
